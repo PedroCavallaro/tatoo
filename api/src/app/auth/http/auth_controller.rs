ba2f13243f29081973550f2e69125e7f6f58c489
PedroCavallaro <pedrolemecavallaro@gmail.com>
2025-05-30 23:06:34 -0300

    feat: add axum

--- api/src/app/auth/http/auth_controller.rs
+++ api/src/app/auth/http/auth_controller.rs
@@ -1,3 +1,37 @@
-pub struct AuthController {}
+use axum::{routing::post, Router};
 
-impl AuthController {}
+use crate::{
+    app::{
+        auth::usecases::login_usecase::LoginUseCase,
+        user::infra::repositories::user_repository::UserRepository,
+    },
+    common::usecase::UseCase,
+    domain::entities::user::User,
+};
+
+use super::dto::login_dto::LoginDTO;
+
+pub struct AuthController {
+    login_usecase: Box<dyn UseCase<User, LoginDTO>>,
+}
+
+impl AuthController {
+    pub fn new() -> Self {
+        let repo = Box::new(UserRepository::new());
+
+        Self {
+            login_usecase: Box::new(LoginUseCase::new(repo)),
+        }
+    }
+
+    pub fn routes(&self) -> Router {
+        // Router::new().route("/login", pos;
+        todo!()
+    }
+}
+
+impl Default for AuthController {
+    fn default() -> Self {
+        Self::new()
+    }
+}
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,5 +1,24 @@
+use std::{env, net::SocketAddr};
+
+use axum::{routing::get, Router};
 use dotenv::dotenv;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
+
+    let port = env::var("PORT")
+        .ok()
+        .and_then(|port| port.parse::<u16>().ok())
+        .unwrap_or(3000);
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind(&SocketAddr::from(([0, 0, 0, 0], port)))
+        .await
+        .unwrap();
+
+    println!("Server running on http://localhost:{:?}", port);
+
+    axum::serve(listener, app).await.unwrap();
 }
