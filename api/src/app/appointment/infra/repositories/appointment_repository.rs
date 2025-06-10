f42f0f2a91a0c5e33ee5804c7b052adeda1cfa9b
PedroCavallaro <pedrolemecavallaro@gmail.com>
2025-06-10 18:47:26 -0300

    feat: add get user appointments

--- api/src/app/appointment/infra/repositories/appointment_repository.rs
+++ api/src/app/appointment/infra/repositories/appointment_repository.rs
@@ -1,5 +1,28 @@
+use diesel::prelude::*;
+
+use crate::{
+    app::appointment::domain::entities::appointment::Appointment,
+    domain::error::ApiError,
+    infra::db::{
+        conn::get_connection,
+        schema::appointment::{dsl::*, user_id},
+    },
+};
+
 pub struct AppointmentRepository {}
 
 impl AppointmentRepository {
-    pub fn get_user_appointments(&self, userId: i64) {}
+    pub fn get_user_appointments(&self, id: i64) -> Result<Vec<Appointment>, ApiError> {
+        let mut conn = get_connection()?;
+
+        let user_appointments = appointment
+            .select(Appointment::as_select())
+            .filter(user_id.eq(id))
+            .load(&mut conn);
+
+        match user_appointments {
+            Ok(val) => Ok(val),
+            Err(_) => Ok(vec![]),
+        }
+    }
 }
